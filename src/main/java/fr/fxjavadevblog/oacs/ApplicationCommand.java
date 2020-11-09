134fe9a308e82d820d66b9dce343e642e2e0317d
fxrobin <fxrobin@gmail.com>
2020-11-09 16:38:52 +0000

    adding everything to be edited with gitpod

--- src/main/java/fr/fxjavadevblog/oacs/ApplicationCommand.java
+++ src/main/java/fr/fxjavadevblog/oacs/ApplicationCommand.java
@@ -1,6 +1,7 @@
-package fr.fxjavadevblog;
+package fr.fxjavadevblog.oacs;
 
 import io.quarkus.picocli.runtime.annotations.TopCommand;
+import lombok.extern.slf4j.Slf4j;
 
 import static picocli.CommandLine.Command;
 
@@ -8,8 +9,11 @@ import static picocli.CommandLine.Command;
 @Command(mixinStandardHelpOptions = true,
         version = "1.0.0"
 )
+
+@Slf4j
 public class ApplicationCommand implements Runnable {
     @Override
     public void run() {
+        log.info("Running the app");
     }
 }
